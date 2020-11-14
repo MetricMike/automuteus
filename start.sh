44af31c34b8d29350d126d3cf5c49edf3fc499bf
Michael Weigle <michael.weigle@gmail.com>
2020-11-14 14:33:00 -0500

    round 2

--- start.sh
+++ start.sh
@@ -2,8 +2,10 @@
 
 go get github.com/mattn/goreman
 go get github.com/automuteus/galactus#
-goreman start -f ProcfileGo
 
+export GALACTUS_EXTERNAL_PORT=443
 export GALACTUS_REDIS_ADDR=${REDIS_URL}
 export AUTOMUTEUS_REDIS_ADDR=${REDIS_URL}
 export BROKER_PORT=${PORT}
+
+goreman start -f ProcfileGo
